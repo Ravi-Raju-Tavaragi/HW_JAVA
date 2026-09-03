77ddc497236d377c4c21c79a8118746c1d40cdcb
ravi <ravirajutavaragi@gmail.com>
2026-09-03 21:24:23 +0530

    Added 03-09-2026 Java programs

---
+++ 02-09-2026/NumRad.java
@@ -0,0 +1,9 @@
+public class NumRad 
+{
+    public static void main(String[] args) 
+    {
+        
+        
+    }
+    
+}
---
+++ 03-09-2026/UsingLoop.java
@@ -0,0 +1,59 @@
+public class UsingLoop
+{
+    public static void main(String[]args)
+    {
+        System.out.println("Increment By 3 Numbers");
+        for(int num = 1; num <= 50; num+=3)
+        {
+            System.out.println(num);
+        }
+
+        System.out.println("Decrement By 5 Numbers");
+        for(int num1 = 100; num1 >= 0; num1-=5)
+        {
+            System.out.println(num1);
+        }
+
+        System.out.println("Printing numbers 1 to 100");
+        for(int num3 = 1; num3 <= 100; num3++)
+        {
+            System.out.println(num3);
+        }
+
+        System.out.println("Print Squres of Number -->10");
+        for(int num4 = 1; num4 <= 10; num4++)
+        {
+            int result = num4 * num4;
+            System.out.println("Square of Number :"+result);
+        }
+
+        System.out.println("Print Cubes of Number -->10");
+        for(int num5 = 1; num5 <= 10; num5++)
+        {
+            int result1 = num5 * num5 *num5;
+            System.out.println("Cube of Number :"+result1);
+        }
+
+        System.out.println("Print Multiple of 5");
+        for(int num6 = 1; num6 <=10; num6++)
+        {
+            int result3 = 5 * num6;
+            System.out.println("5 multiple :"+result3);
+        }
+
+        System.out.println("Reverse Alphabet");
+        for(char ch = 'Z'; ch >= 'A'; ch--)
+        {
+            System.out.println(ch);
+        }
+
+        System.out.println("Sum of N numbers");
+        for(int num7 = 0; num7 <= 10; num7++)
+        {
+            int result4 = num7 + num7;
+            System.out.println("Sum of Numbers :"+result4);
+        }
+
+    }
+    
+}
