d4b8a8e366f68e02a53a40983f0480c2aaf04b7e
ravi <ravirajutavaragi@gmail.com>
2026-09-12 18:50:45 +0530

    Added MethodPractice Java programs

---
+++ LoopPractice/DaimondPattern.java
@@ -0,0 +1,17 @@
+package LoopPractice;
+
+public class DaimondPattern 
+{
+    public static void main(String[]args)
+    {
+        for(int i = 1; i <= 5; i++)
+        {
+            for(int j = 1; j <= 5; j++)
+            {
+                j = 2 * i - 1;
+                System.out.print(j);
+            }
+        }
+    }
+    
+}
