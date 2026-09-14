4dd7d0d8dbc9daa9a79991b825e775171cac1124
ravi <ravirajutavaragi@gmail.com>
2026-09-14 22:41:17 +0530

    Added MethodPractice Java programs

---
+++ Mon14SEP/PrimeNumber.java
@@ -0,0 +1,20 @@
+package Mon14SEP;
+
+import java.util.Scanner;
+
+public class PrimeNumber
+{
+
+
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number :");
+        int num = sc.nextInt();
+
+
+        sc.close();
+    }
+    
+}
