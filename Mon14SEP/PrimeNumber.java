fd121627889331486f734b8f524cddb606656008
ravi <ravirajutavaragi@gmail.com>
2026-09-14 22:45:22 +0530

    Added MethodPractice Java programs

--- Mon14SEP/PrimeNumber.java
+++ Mon14SEP/PrimeNumber.java
@@ -12,6 +12,24 @@ public class PrimeNumber
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Number :");
         int num = sc.nextInt();
+                boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i * i <= n; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(n + " is a prime number");
+        } else {
+            System.out.println(n + " is not a prime number");
+        }
 
 
         sc.close();
