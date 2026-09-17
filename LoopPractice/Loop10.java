2544dc9bce75932834de770d4123230634c85ccc
ravi <ravirajutavaragi@gmail.com>
2026-09-17 22:00:29 +0530

    Aded Festival home work Practice Java programs

---
+++ LoopPractice/Loop10.java
@@ -0,0 +1,18 @@
+package LoopPractice;
+
+public class Loop10
+{
+    public static void main(String[] args)
+    {
+         for (int i = 1; i <= 5; i++) 
+        {
+            System.out.print(i + " ");
+
+            if (i == 2) 
+            {
+                i++;
+            }
+        }
+    }
+    
+}
