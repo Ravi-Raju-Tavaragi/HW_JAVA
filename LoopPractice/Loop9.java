e31a61f1f43d47844cf2ee5d55006201dbeb3a9c
ravi <ravirajutavaragi@gmail.com>
2026-09-17 21:57:59 +0530

    Aded Festival home work Practice Java programs

---
+++ LoopPractice/Loop9.java
@@ -0,0 +1,24 @@
+package LoopPractice;
+
+public class Loop9
+{
+    public static void main(String[] args) 
+    {
+        for (int i = 1; i <= 4; i++) 
+        {
+            for (int j = 1; j <= 4; j++) 
+            {
+                if (j == 3) 
+                {
+                    break;
+                }
+
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+
+    }
+    
+}
